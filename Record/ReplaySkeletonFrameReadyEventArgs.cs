c5dc81f9c3b11ab0f9cd41e142e6fe963afca336
Xiaolong Jiang <klose@gwmail.gwu.edu>
2012-05-09 17:32:07 -0400

    record function
    may not use it in the final version

---
+++ Record/ReplaySkeletonFrameReadyEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace Kinect.Toolbox.Record
+{
+    public class ReplaySkeletonFrameReadyEventArgs : EventArgs
+    {
+        public ReplaySkeletonFrame SkeletonFrame { get; set; }
+    }
+}
